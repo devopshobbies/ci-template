fe2399cbbf2d859afe64581239270805b8de47fa
mohammadne <mohammadne.dev@gmail.com>
2023-04-12 02:35:14 +0330

    feat: add skeleton of the project

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# simple-ci
-simple demonstration of popular CI/CD tools
+# Simple CI
+
+In this project we aimed to demonstrate how to use different and popular CI/CD tools for a simple Go application.
---
+++ main_test.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestTableCalculate(t *testing.T) {
+	var tests = []struct {
+		input    int
+		expected int
+	}{
+		{2, 4},
+		{-1, 1},
+		{0, 2},
+		{-5, -3},
+		{99999, 100001},
+	}
+
+	for _, test := range tests {
+		if output := Calculate(test.input); output != test.expected {
+			t.Error("Test Failed: {} inputted, {} expected, recieved: {}", test.input, test.expected, output)
+		}
+	}
+}
